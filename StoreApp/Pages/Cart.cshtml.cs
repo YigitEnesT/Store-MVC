412b3ec4fe78647fb369a9a2f5fc6b3545b38f40
YigitEnesTuncay <yigit.enes.tuncay@outlook.com>
2024-07-29 12:30:00 +0300

    Session

---
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -0,0 +1,48 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
+using StoreApp.Infrastracture.Extentions;
+
+namespace StoreApp.Pages
+{
+    public class CartModel : PageModel 
+    {
+        private readonly IServiceManager _manager;
+
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+
+        public CartModel(IServiceManager manager, Cart cartService)
+        {
+            _manager = manager;
+            Cart = cartService;
+        }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+        }
+        public IActionResult OnPost(int productId, string returnUrl)
+        {
+            Product? product = _manager.ProductService.GetOneProduct(productId,false);
+            if(product is not null)
+            {
+                Cart.AddItem(product,1); 
+            }    
+
+            return Page();
+        }
+
+        public IActionResult OnPostRemove(int id, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
+            return Page();
+        }
+        public IActionResult OnPostRemoveAll()
+        {
+            Cart.Clear();
+            return Page();
+        }
+    }
+}
