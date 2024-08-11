933c81f4a52847c44ca13710e421a2bdc0fc8066
YigitEnesTuncay <yigit.enes.tuncay@outlook.com>
2024-08-11 12:30:00 +0300

    Carousel
    Carousel added to Showcase

--- StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef884cdde942ba2d0ed948e34bbd66d90f4d8e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09c511495b144983b1203843fd9a220f92def7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
